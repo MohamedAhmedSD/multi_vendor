e164f5129839a890417fcf5295606e2bef6745cd
MohamedAhmedSD <btoox8python@gmail.com>
2023-04-19 22:18:36 +0200

    [login form && controller]

--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -1,80 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:multi_vendor/controllers/auth_controller.dart';
 import 'package:multi_vendor/views/auth/register_screen.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late String email, password;
+  //! we use onchange && validator
+  //? wrap under form, use globalkey && auth controller
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final AuthController _authController = AuthController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
           child: SingleChildScrollView(
-            child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-            Text(
-              "Login Customer's Account",
-              style: TextStyle(fontSize: 20),
-            ),
-                   
-            Padding(
-              padding: EdgeInsets.all(13),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Enter Email",
-                ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Login Customer's Account",
+                style: TextStyle(fontSize: 20),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(13),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Password",
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            //! button
-            Container(
-              width: MediaQuery.of(context).size.width - 40,
-              height: 50,
-              decoration: BoxDecoration(
-                color: Colors.yellow.shade900,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 19,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 4),
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Need an account?"),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) {
-                        return RegisterScreen();
-                      }),
-                    );
+
+              Padding(
+                padding: EdgeInsets.all(13),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please email must not be empty";
+                    } else {
+                      return null;
+                    }
                   },
-                  child: Text("Login"),
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Enter Email",
+                  ),
                 ),
-              ],
-            )
-                  ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(13),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please password must not be empty";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                  ),
                 ),
-          )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              //! button
+              Container(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.yellow.shade900,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 19,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 4),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Need an account?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) {
+                          return RegisterScreen();
+                        }),
+                      );
+                    },
+                    child: Text("Login"),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
